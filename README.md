a19b7a4104433b758a0963bd7eb0002c3d821baa
Nicolas Faure <nicolas.faure.7@gmail.com>
2014-09-22 10:44:30 +0800

    Added documentation. Added working cover/uncover switch. Added position setting.

--- README.md
+++ README.md
@@ -3,6 +3,40 @@
 ## About
 Plugin for cycle2 implementing cycle1's cover and uncover transitions.
 
+## Getting started
+```html
+<!-- include jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<!-- include Cycle2 -->
+<script src="http://malsup.github.com/jquery.cycle2.js"></script>
+<!-- include Cover transition -->
+<script src="https://raw.githubusercontent.com/Spark-NF/cycle2-cover/master/jquery.cycle2.cover.js"></script>
+
+...
+
+<!-- declare a slideshow -->
+<div class="cycle-slideshow"
+    data-cycle-fx="cover"
+    data-cycle-cover-enter-animation="cover"
+    data-cycle-cover-enter-position="top"
+    data-cycle-cover-exit-animation="uncover"
+    data-cycle-cover-exit-position="bottom">
+
+    <img src="http://malsup.github.com/images/p1.jpg">
+    <img src="http://malsup.github.com/images/p2.jpg">
+    <img src="http://malsup.github.com/images/p3.jpg">
+</div>
+```
+
+## Documentation
+Name            | Type   | Default value | Description
+--------------- | ------ | ------------- | -----------
+enter-animation | string | cover         | Whether the slide will cover the previous one or if the previous one should leave to show the next one. Can be 'cover' or 'uncover'.
+enter-position  | string | left          | Where the slide will enter from. Can be 'top', 'right', 'bottom' or 'left'.
+exit-animation  | string | cover         | Whether the slide will cover the previous one or if the previous one should leave to show the next one. Can be 'cover' or 'uncover'.
+exit-position   | string | right         | Where the slide will exit to. Can be 'top', 'right', 'bottom' or 'left'.
+The 'easing' and 'speed' options are also supported and work the same as in the original Cycle2 (see [Cycle2 API](http://jquery.malsup.com/cycle2/api/)).
+
 ## Authors
 * Nicolas Faure
 
--- jquery.cycle2.cover.js
+++ jquery.cycle2.cover.js
@@ -3,32 +3,61 @@
 	
 	$.fn.cycle.transitions.cover = {
 	    transition: function( opts, currEl, nextEl, fwd, callback ) {
+	    	var uncover = false;
+	        if ((fwd && opts.coverEnterAnimation == 'uncover')
+	        	|| (!fwd && opts.coverExitAnimation == 'uncover')) {
+	        	uncover = true;
+	        }
+
 	        $( nextEl ).css({
 	            display: 'block',
 	            visibility: 'visible'
 	        });
+	        opts.container.css('overflow', 'hidden');
 	        var width = opts.container.width();
 	        var height = opts.container.height();
 	        var speed = opts.speed;
-	        var element = nextEl;
+	        var element = uncover ? currEl : nextEl;
 
-	        opts = opts.API.getSlideOpts( opts.nextSlide );
-	        var props1 = { left: fwd ? -width : width, top: 0 };
-	        if (opts.direction == 'up') {
-	        	props1 = { left: 0, top: fwd ? -height : height };
+	        opts = opts.API.getSlideOpts( uncover ? opts.currSlide : opts.nextSlide );
+	        var props1 = { left: -width, top: 0 };
+	        if (fwd) {
+		        if (opts.coverEnterPosition == 'top') {
+		        	props1 = { left: 0, top: -height };
+		        }
+		        else if (opts.coverEnterPosition == 'right') {
+		        	props1 = { left: width, top: 0 };
+		        }
+		        else if (opts.coverEnterPosition == 'bottom') {
+		        	props1 = { left: 0, top: height };
+		        }
 	        }
-	        else if (opts.direction == 'right') {
-	        	props1 = { left: fwd ? width : -width, top: 0 };
-	        }
-	        else if (opts.direction == 'down') {
-	        	props1 = { left: 0, top: fwd ? height : -height };
+	        else {
+		        props1 = { left: width, top: 0 };
+		        if (opts.coverExitPosition == 'top') {
+		        	props1 = { left: 0, top: -height };
+		        }
+		        else if (opts.coverExitPosition == 'left') {
+		        	props1 = { left: -width, top: 0 };
+		        }
+		        else if (opts.coverExitPosition == 'bottom') {
+		        	props1 = { left: 0, top: height };
+		        }
 	        }
 	        var props2 = { left: 0, top: 0 };
 
-	        $(element)
-	            .css( props1 )
-	            .queue( 'fx', $.proxy(reIndex, this))
-	            .animate( props2, speed, opts.easing, callback );
+	        if (uncover) {
+		        $(element)
+		            .animate(props1, speed, opts.easing, callback)
+		            .queue('fx', $.proxy(reIndex, this))
+		            .animate(props2, 0, opts.easing, callback);
+	        }
+	        else {
+		        $(element)
+		            .css(props1)
+		            .queue('fx', $.proxy(reIndex, this))
+		            .animate(props2, speed, opts.easing, callback);
+	        }
 
 	        function reIndex(nextFn) {
 	            this.stack(opts, currEl, nextEl, fwd);
@@ -39,7 +68,13 @@
 	    stack: function( opts, currEl, nextEl, fwd ) {
 	        var i, z;
 
-	        if (fwd) {
+	        var f = fwd;
+	        if ((fwd && opts.coverEnterAnimation == 'uncover')
+	        	|| (!fwd && opts.coverExitAnimation == 'uncover')) {
+	        	f = !fwd;
+	        }
+
+	        if (f) {
 	            z = 1;
 	            for (i = opts.nextSlide - 1; i >= 0; i--) {
 	                $(opts.slides[i]).css('zIndex', z++);
@@ -47,12 +82,12 @@
 	            for (i = opts.slideCount - 1; i > opts.nextSlide; i--) {
 	                $(opts.slides[i]).css('zIndex', z++);
 	            }
-	            $(nextEl).css( 'zIndex', opts.maxZ );
-	            $(currEl).css( 'zIndex', opts.maxZ - 1 );
+	            $(nextEl).css('zIndex', opts.maxZ);
+	            $(currEl).css('zIndex', opts.maxZ - 1);
 	        }
 	        else {
 	            opts.API.stackSlides( nextEl, currEl, fwd );
-	            $(currEl).css( 'zIndex', 1 );
+	            $(currEl).css('zIndex', 1);
 	        }
 	    }
 
